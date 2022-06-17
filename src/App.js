2b9a95186899f8df224bdbaad3335a182c9d7e2f
trevorfaivre <trevorfaivre@hotmail.com>
2022-06-16 22:08:32 -0500

    FIN

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
 import './App.css';
+import Doggo from "./components/Doggo";
+// import Form from "./components/Form";
 
 function App() {
+  const API_URL= "https://dog.ceo/api/breeds/image/random";
+
+  const [dog, setDog] = useState(null);
+  
+  const getDog = async () => {
+    const response = await fetch(API_URL);
+
+    const data = await response.json();
+    setDog(data.message);
+  };
+
+  useEffect(() => {
+    getDog();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="tag">Random Doggos!</div>
+      <div className="submit">
+        <input type="button" value="Get a Dog" onClick={getDog} />
+      </div>
+      <div className="dogImg">
+        <Doggo dog={dog} />
+      </div>
     </div>
   );
-}
+  }
+//   return (
+//     <div className="App">
+//       <h1>Random Dog For To Make A Day Happy</h1>
+//       <Form dogsearch={getDoggo} />
+//       <Doggo doggo={doggo} />
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/components/Doggo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Doggo = ({dog}) => {
+    return <img src={dog} alt= "random" />
+    };
+
+export default Doggo;
